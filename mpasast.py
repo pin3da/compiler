ff7a26d0f92fbdf426b92fd687996c6b5ad3843e
Carlos <caal.0522@gmail.com>
2013-04-12 10:50:42 -0500

    espacios no tabs

--- mpasast.py
+++ mpasast.py
@@ -13,37 +13,37 @@ añadir más.
 
 # NO MODIFICAR
 class AST(object):
-	'''
-	Clase base para todos los nodos del AST.  Cada nodo se espera 
-	definir el atributo _fields el cual enumera los nombres de los
-	atributos almacenados.  El método a continuación __init__() toma
-	argumentos posicionales y los asigna a los campos apropiados.
-	Cualquier argumento adicional especificado como keywords son 
-	también asignados.
-	'''
-	_fields = []
-	def __init__(self,*args,**kwargs):
-		assert len(args) == len(self._fields)
-		for name,value in zip(self._fields,args):
-			setattr(self,name,value)
-		# Asigna argumentos adicionales (keywords) si se suministran
-		for name,value in kwargs.items():
-			setattr(self,name,value)
+    '''
+    Clase base para todos los nodos del AST.  Cada nodo se espera 
+    definir el atributo _fields el cual enumera los nombres de los
+    atributos almacenados.  El método a continuación __init__() toma
+    argumentos posicionales y los asigna a los campos apropiados.
+    Cualquier argumento adicional especificado como keywords son 
+    también asignados.
+    '''
+    _fields = []
+    def __init__(self,*args,**kwargs):
+        assert len(args) == len(self._fields)
+        for name,value in zip(self._fields,args):
+            setattr(self,name,value)
+        # Asigna argumentos adicionales (keywords) si se suministran
+        for name,value in kwargs.items():
+            setattr(self,name,value)
 
-	def pprint(self):
-		for depth, node in flatten(self):
-			print("%s%s" % (" "*(4*depth),node))
+    def pprint(self):
+        for depth, node in flatten(self):
+            print("%s%s" % (" "*(4*depth),node))
 
 def validate_fields(**fields):
-	def validator(cls):
-		old_init = cls.__init__
-		def __init__(self, *args, **kwargs):
-			old_init(self, *args, **kwargs)
-			for field,expected_type in fields.items():
-				assert isinstance(getattr(self, field), expected_type)
-		cls.__init__ = __init__
-		return cls
-	return validator
+    def validator(cls):
+        old_init = cls.__init__
+        def __init__(self, *args, **kwargs):
+            old_init(self, *args, **kwargs)
+            for field,expected_type in fields.items():
+                assert isinstance(getattr(self, field), expected_type)
+        cls.__init__ = __init__
+        return cls
+    return validator
 
 # ----------------------------------------------------------------------
 # Nodos AST especificos
@@ -60,150 +60,150 @@ def validate_fields(**fields):
 # Unos pocos nodos ejemplos
 
 class PrintStatement(AST):
-	'''
-	print expression ;
-	'''
-	_fields = ['expr']
+    '''
+    print expression ;
+    '''
+    _fields = ['expr']
     '''
 class Literal(AST):
-	'''
-	#Un valor constante como 2, 2.5, o "dos"
-	'''
-	_fields = ['value']
+    '''
+    #Un valor constante como 2, 2.5, o "dos"
+    '''
+    _fields = ['value']
 '''
 
 #Revisado
 @validate_fields(functions=list)
 class Program(AST):
-	_fields = ['functions']
+    _fields = ['functions']
 
-	def append(self,e):
-		sef.functions.append(e)
+    def append(self,e):
+        sef.functions.append(e)
 #Revisado
 @validate_fields(instructions=list)
 class Statement(AST):
-	_fields = ['instructions']
+    _fields = ['instructions']
 
-	def append(self,e):
-		self.instructions.append(e)
-#Revisado	
+    def append(self,e):
+        self.instructions.append(e)
+#Revisado    
 class Instruction(AST):
-	_fields = ['instruction']
+    _fields = ['instruction']
 
 '''
 class Extern(AST):
-	_fields = ['func_prototype']
+    _fields = ['func_prototype']
 '''
 #Revisado
 class Function(AST):
-	_fields = ['arglist', 'typename', 'locals', 'block']
+    _fields = ['arglist', 'typename', 'locals', 'block']
 #Revisado
 @validate_fields(variables=list)
 class Arg_list(AST):
-	_fields = ['variables']
+    _fields = ['variables']
 
-	def append(self,e):
-		self.variables.append(e)
+    def append(self,e):
+        self.variables.append(e)
 #Revisado
 class Var(AST):
-	_fields = ['id', 'typename']
+    _fields = ['id', 'typename']
 
 #Revisado
 class Var_dec_as(AST):
-	_fields = ['value']
+    _fields = ['value']
 
 #R
 @validate_fields(local_list=list)
 class Locals(AST):
-	_fields = ['local_list']
-	
-	def append(self,e):
-		self.local_list.append(e)
+    _fields = ['local_list']
+    
+    def append(self,e):
+        self.local_list.append(e)
 
 #Revisado
 class Cwhile(AST):
-	_fields =['conditional', 'block']
+    _fields =['conditional', 'block']
 
 #Revisado
 class Cif(AST):
-	_fields =['conditional', 'block', 'else']
+    _fields =['conditional', 'block', 'else']
 
 #Revisado
 class Else(AST):
-	_fields =['statement']
+    _fields =['statement']
 
 #R
 class Prod(AST):
-	_fields['value']
+    _fields['value']
 #R
 class Term(AST):
-	_fields['value']
+    _fields['value']
 #R?
 class Return(AST):
-	_fields['value'] #solo se puede retornar una expresion?
+    _fields['value'] #solo se puede retornar una expresion?
 
 #R
 class PrintExpression(AST):
-	_fields=['expression']
+    _fields=['expression']
 
 #R
 class PrintString(AST):
-	_fields=['value']
+    _fields=['value']
 
 #R
 class Call_d(AST):
-	_fields=['funcid', 'varlist']
+    _fields=['funcid', 'varlist']
 
 #R
 class Line_if(AST):
-	_fields=['condition', 'statement']
+    _fields=['condition', 'statement']
 
 #R
 class Line_while(AST):
-	_fields=['condition', 'statement']
-	
+    _fields=['condition', 'statement']
+    
 
 class Assignation(AST):
-	_fields = ['id', 'value']
+    _fields = ['id', 'value']
 
 class ConstDeclaration(AST):
-	_fields = ['id', 'value']
+    _fields = ['id', 'value']
 
 class VarDeclaration(AST):
-	_fields = ['id', 'typename', 'value']
+    _fields = ['id', 'typename', 'value']
 
 
 class LoadLocation(AST):
-	_fields = ['name']
+    _fields = ['name']
 
 class StoreVar(AST):
-	_fields = ['name']
+    _fields = ['name']
 
 #Revisado, operaciones Unarias
 class UnaryOp(AST):
-	_fields = ['op', 'left']
+    _fields = ['op', 'left']
 
 #Revisado, operaciones aritmeticas, comparativas
 class BinaryOp(AST):
-	_fields = ['op', 'left', 'right']
+    _fields = ['op', 'left', 'right']
 
 #Revisado, operaciones lógicas, falta soporte en Parse
 class RelationalOp(AST):
-	_fields = ['op', 'left', 'right']
+    _fields = ['op', 'left', 'right']
 
 
 class Group(AST):
-	_fields = ['expression']
+    _fields = ['expression']
 
 
 class ExprList(AST):
-	_fields = ['expressions']
+    _fields = ['expressions']
 
-	def append(self, e):
-		self.expressions.append(e)
+    def append(self, e):
+        self.expressions.append(e)
 
 class Empty(AST):
-	_fields = []
+    _fields = []
 
 
 # Usted deberá añadir mas nodos aquí.  Algunos nodos sugeridos son
@@ -219,109 +219,109 @@ class Empty(AST):
 
 # NO MODIFIQUE
 class NodeVisitor(object):
-	'''
-	Clase para visitar nodos del árbol de sintaxis.  Se modeló a partir
-	de una clase similar en la librería estándar ast.NodeVisitor.  Para
-	cada nodo, el método visit(node) llama un método visit_NodeName(node)
-	el cual debe ser implementado en la subclase.  El método genérico
-	generic_visit() es llamado para todos los nodos donde no hay coincidencia
-	con el método visit_NodeName().
-	
-	Es es un ejemplo de un visitante que examina operadores binarios:
+    '''
+    Clase para visitar nodos del árbol de sintaxis.  Se modeló a partir
+    de una clase similar en la librería estándar ast.NodeVisitor.  Para
+    cada nodo, el método visit(node) llama un método visit_NodeName(node)
+    el cual debe ser implementado en la subclase.  El método genérico
+    generic_visit() es llamado para todos los nodos donde no hay coincidencia
+    con el método visit_NodeName().
+    
+    Es es un ejemplo de un visitante que examina operadores binarios:
 
-		class VisitOps(NodeVisitor):
-			visit_Binop(self,node):
-				print("Operador binario", node.op)
-				self.visit(node.left)
-				self.visit(node.right)
-			visit_Unaryop(self,node):
-				print("Operador unario", node.op)
-				self.visit(node.expr)
+        class VisitOps(NodeVisitor):
+            visit_Binop(self,node):
+                print("Operador binario", node.op)
+                self.visit(node.left)
+                self.visit(node.right)
+            visit_Unaryop(self,node):
+                print("Operador unario", node.op)
+                self.visit(node.expr)
 
-		tree = parse(txt)
-		VisitOps().visit(tree)
-	'''
-	def visit(self,node):
-		'''
-		Ejecuta un método de la forma visit_NodeName(node) donde
-		NodeName es el nombre de la clase de un nodo particular.
-		'''
-		if node:
-			method = 'visit_' + node.__class__.__name__
-			visitor = getattr(self, method, self.generic_visit)
-			return visitor(node)
-		else:
-			return None
-	
-	def generic_visit(self,node):
-		'''
-		Método ejecutado si no se encuentra médodo aplicable visit_.
-		Este examina el nodo para ver si tiene _fields, es una lista,
-		o puede ser recorrido completamente.
-		'''
-		for field in getattr(node,"_fields"):
-			value = getattr(node,field,None)
-			if isinstance(value, list):
-				for item in value:
-					if isinstance(item,AST):
-						self.visit(item)
-			elif isinstance(value, AST):
-				self.visit(value)
+        tree = parse(txt)
+        VisitOps().visit(tree)
+    '''
+    def visit(self,node):
+        '''
+        Ejecuta un método de la forma visit_NodeName(node) donde
+        NodeName es el nombre de la clase de un nodo particular.
+        '''
+        if node:
+            method = 'visit_' + node.__class__.__name__
+            visitor = getattr(self, method, self.generic_visit)
+            return visitor(node)
+        else:
+            return None
+    
+    def generic_visit(self,node):
+        '''
+        Método ejecutado si no se encuentra médodo aplicable visit_.
+        Este examina el nodo para ver si tiene _fields, es una lista,
+        o puede ser recorrido completamente.
+        '''
+        for field in getattr(node,"_fields"):
+            value = getattr(node,field,None)
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item,AST):
+                        self.visit(item)
+            elif isinstance(value, AST):
+                self.visit(value)
 
 # NO MODIFICAR
 class NodeTransformer(NodeVisitor):
-	'''
-	Clase que permite que los nodos del arbol de sintraxis sean 
-	reemplazados/reescritos.  Esto es determinado por el valor retornado
-	de varias funciones visit_().  Si el valor retornado es None, un
-	nodo es borrado. Si se retorna otro valor, reemplaza el nodo
-	original.
-	
-	El uso principal de esta clase es en el código que deseamos aplicar
-	transformaciones al arbol de sintaxis.  Por ejemplo, ciertas optimizaciones
-	del compilador o ciertas reescrituras de pasos anteriores a la generación
-	de código.
-	'''
-	def generic_visit(self,node):
-		for field in getattr(node,"_fields"):
-			value = getattr(node,field,None)
-			if isinstance(value,list):
-				newvalues = []
-				for item in value:
-					if isinstance(item,AST):
-						newnode = self.visit(item)
-						if newnode is not None:
-							newvalues.append(newnode)
-					else:
-						newvalues.append(n)
-				value[:] = newvalues
-			elif isinstance(value,AST):
-				newnode = self.visit(value)
-				if newnode is None:
-					delattr(node,field)
-				else:
-					setattr(node,field,newnode)
-		return node
+    '''
+    Clase que permite que los nodos del arbol de sintraxis sean 
+    reemplazados/reescritos.  Esto es determinado por el valor retornado
+    de varias funciones visit_().  Si el valor retornado es None, un
+    nodo es borrado. Si se retorna otro valor, reemplaza el nodo
+    original.
+    
+    El uso principal de esta clase es en el código que deseamos aplicar
+    transformaciones al arbol de sintaxis.  Por ejemplo, ciertas optimizaciones
+    del compilador o ciertas reescrituras de pasos anteriores a la generación
+    de código.
+    '''
+    def generic_visit(self,node):
+        for field in getattr(node,"_fields"):
+            value = getattr(node,field,None)
+            if isinstance(value,list):
+                newvalues = []
+                for item in value:
+                    if isinstance(item,AST):
+                        newnode = self.visit(item)
+                        if newnode is not None:
+                            newvalues.append(newnode)
+                    else:
+                        newvalues.append(n)
+                value[:] = newvalues
+            elif isinstance(value,AST):
+                newnode = self.visit(value)
+                if newnode is None:
+                    delattr(node,field)
+                else:
+                    setattr(node,field,newnode)
+        return node
 
 # NO MODIFICAR
 def flatten(top):
-	'''
-	Aplana el arbol de sintaxis dentro de una lista para efectos
-	de depuración y pruebas.  Este retorna una lista de tuplas de
-	la forma (depth, node) donde depth es un entero representando
-	la profundidad del arból de sintaxis y node es un node AST
-	asociado.
-	'''
-	class Flattener(NodeVisitor):
-		def __init__(self):
-			self.depth = 0
-			self.nodes = []
-		def generic_visit(self,node):
-			self.nodes.append((self.depth,node))
-			self.depth += 1
-			NodeVisitor.generic_visit(self,node)
-			self.depth -= 1
+    '''
+    Aplana el arbol de sintaxis dentro de una lista para efectos
+    de depuración y pruebas.  Este retorna una lista de tuplas de
+    la forma (depth, node) donde depth es un entero representando
+    la profundidad del arból de sintaxis y node es un node AST
+    asociado.
+    '''
+    class Flattener(NodeVisitor):
+        def __init__(self):
+            self.depth = 0
+            self.nodes = []
+        def generic_visit(self,node):
+            self.nodes.append((self.depth,node))
+            self.depth += 1
+            NodeVisitor.generic_visit(self,node)
+            self.depth -= 1
 
-	d = Flattener()
-	d.visit(top)
-	return d.nodes
+    d = Flattener()
+    d.visit(top)
+    return d.nodes
--- mpasparse.py
+++ mpasparse.py
@@ -9,173 +9,173 @@ from mpaslex import tokens
 from mpasast import *
 
 def p_program(p):
-	'''
-	program : function
-			| function program
-	'''
-	if(len(p) == 2):
-		p[0] = Program(p[1])
-	else:
-		p[2].append(p[1])
-		p[0] = p[2]
+    '''
+    program : function
+            | function program
+    '''
+    if(len(p) == 2):
+        p[0] = Program(p[1])
+    else:
+        p[2].append(p[1])
+        p[0] = p[2]
 
 def p_function(p):
-	'''
-	function : FUNC ID LPAREN arg_list RPAREN return_f locals block
-			 | FUNC ID LPAREN RPAREN return_f locals block
-	'''
-	if(len (p) == 9):
-		p[0] = Function(p[4],p[6],p[7],p[8])
-	else:
-		p[0] = Function(None,p[6],p[7],p[8])
+    '''
+    function : FUNC ID LPAREN arg_list RPAREN return_f locals block
+             | FUNC ID LPAREN RPAREN return_f locals block
+    '''
+    if(len (p) == 9):
+        p[0] = Function(p[4],p[6],p[7],p[8])
+    else:
+        p[0] = Function(None,p[6],p[7],p[8])
 
 def p_empty(p):
-	'''
-	empty :
-	'''
-	pass
+    '''
+    empty :
+    '''
+    pass
 
 def p_return_f(p):
-	'''
-	return_f : COLON type
-			 | empty
-	'''
-	if(len(p) == 3):
-		p[0] = p[2]
-	else
-		p[0] = None
-		#Revisar.
+    '''
+    return_f : COLON type
+             | empty
+    '''
+    if(len(p) == 3):
+        p[0] = p[2]
+    else
+        p[0] = None
+        #Revisar.
 
 def p_block(p):
-	'''
-	block: BEGIN statements END
-	'''
-	p[0] = Block(p[2])
+    '''
+    block: BEGIN statements END
+    '''
+    p[0] = Block(p[2])
 
 def p_arg_list(p):
-	'''
-	arg_list : var COMMA arg_list
-			 | var
-	'''
-	if(len(p) == 4 ):
-		p[3].append(p[1])
-		p[0] = p[1]
-	else:
-		p[0] = Arg_list(p[1])
+    '''
+    arg_list : var COMMA arg_list
+             | var
+    '''
+    if(len(p) == 4 ):
+        p[3].append(p[1])
+        p[0] = p[1]
+    else:
+        p[0] = Arg_list(p[1])
 
 
 def p_locals(p):
-	'''
-	locals : local_list SEMICOLON locals
-		   | empty
-	'''
-	if( len(p) == 4):
-		p[3].append(p[1])
-		p[0] = p[3]
-	else:
-		p[0] = Locals(p[1].local_list)#nuevo no se esto que tal
-		#Hay que hacer un nodo pa esto, o espera que el metodo append aparezca por obra y gracia del espiritu santo?? jajajaja
+    '''
+    locals : local_list SEMICOLON locals
+           | empty
+    '''
+    if( len(p) == 4):
+        p[3].append(p[1])
+        p[0] = p[3]
+    else:
+        p[0] = Locals([])#nuevo no se esto que tal
+        #Hay que hacer un nodo pa esto, o espera que el metodo append aparezca por obra y gracia del espiritu santo?? jajajaja
 
 def p_local_list(p):
-	'''
-	local_list : var
-			   | var_dec_as
-	'''
-	p[0] = p[1]#?
+    '''
+    local_list : var
+               | var_dec_as
+    '''
+    p[0] = p[1]#?
 
 
 def p_var(p):
-	'''
-	var : ID COLON type
-	'''
-	p[0] =  Var(p[1],p[3])
-	# El type y name se definen en el constructor del nodo.
-	# TYPE deberia ser una produccion.
+    '''
+    var : ID COLON type
+    '''
+    p[0] =  Var(p[1],p[3])
+    # El type y name se definen en el constructor del nodo.
+    # TYPE deberia ser una produccion.
 
 def p_var_dec_as(p):
-	'''
-	var_dec_as : ID COLONEQUAL value_type 
-			   | ID COLONEQUAL expression
-	'''
-	p[0] = Var_dec_as(p[1],p[3]) #value_type seria como los tipos de valores que hay booleano, int, etc, el valor comotal
-	# Es mejor separarlas ?
-	#parce porque type o expression, esto que hace exactamente?
+    '''
+    var_dec_as : ID COLONEQUAL value_type 
+               | ID COLONEQUAL expression
+    '''
+    p[0] = Var_dec_as(p[1],p[3]) #value_type seria como los tipos de valores que hay booleano, int, etc, el valor comotal
+    # Es mejor separarlas ?
+    #parce porque type o expression, esto que hace exactamente?
 def p_statement(p):
-	'''
-	statement : controlstructure SEMICOLON statement
-			  | instruction SEMICOLON statement
-			  | empty
-	'''	
-	if( len(p) == 4 ):
-		p[3].append(p[1])
-		p[0] = p[3]
-	else:
-		p[0] = Statement(p[0].instructions) #a lo mejor Statement p[0].instructions, para llenar los fields 
-		#Esto estaba retostado XD, de hecho aun no se si esta bien
+    '''
+    statement : controlstructure SEMICOLON statement
+              | instruction SEMICOLON statement
+              | empty
+    '''    
+    if( len(p) == 4 ):
+        p[3].append(p[1])
+        p[0] = p[3]
+    else:
+        p[0] = Statement([]) #a lo mejor Statement p[0].instructions, para llenar los fields 
+        #Esto estaba retostado XD, de hecho aun no se si esta bien
 
 def p_controlstructure(p): #ahorrandose reglas a toda hora home jajajaja
-	'''
-	controlstructure : WHILE  LPAREN conditional RPAREN DO block SEMICOLON
-					 |  IF LPAREN conditional RPAREN THEN block else SEMICOLON
-	'''
-	if( len(p) == 6):
-		p[0] = Cwhile(p[3],p[6])
-	else:
-		p[0] = Cif(p[3],p[6],p[7]) 
+    '''
+    controlstructure : WHILE  LPAREN conditional RPAREN DO block SEMICOLON
+                     |  IF LPAREN conditional RPAREN THEN block else SEMICOLON
+    '''
+    if( len(p) == 6):
+        p[0] = Cwhile(p[3],p[6])
+    else:
+        p[0] = Cif(p[3],p[6],p[7]) 
 
 def p_else(p)
-	'''
-	else : ELSE statement
-		 | empty
-	'''
-	if ( len(p) == 3):
-		p[0] = Else(p[2])
-	else:
-		p[0] = None
+    '''
+    else : ELSE statement
+         | empty
+    '''
+    if ( len(p) == 3):
+        p[0] = Else(p[2])
+    else:
+        p[0] = None
 
 def p_conditional(p):
-	'''
-	conditional :  bool_expr OR bool_expr
-				|  bool_expr AND bool_expr
-				|  NOT bool_expr
-				|  bool_expr
-	'''
-	if( len(p) == 4):
-		if(p[2] == '||' ):
-			p[0] = Or(p[1], p[3])
-		else:
-			p[0] = And(p[1], p[3])
-	elif (len(p) == 3):
-		p[0] = Not(p[2])
-	else:
-		p[0] = p[1]
-	#Se podria poner como binaryop, o dejarlo en varias clases, yo opto por el Relational Op que ya esta y enviamos ademas el signo?.
+    '''
+    conditional :  bool_expr OR bool_expr
+                |  bool_expr AND bool_expr
+                |  NOT bool_expr
+                |  bool_expr
+    '''
+    if( len(p) == 4):
+        if(p[2] == '||' ):
+            p[0] = Or(p[1], p[3])
+        else:
+            p[0] = And(p[1], p[3])
+    elif (len(p) == 3):
+        p[0] = Not(p[2])
+    else:
+        p[0] = p[1]
+    #Se podria poner como binaryop, o dejarlo en varias clases, yo opto por el Relational Op que ya esta y enviamos ademas el signo?.
 
 def bool_expr(p):
-	'''
-	bool_expr : expression GREATER expression
-			  | expression LESS expression
-			  | expression GREATEREQUAL expression
-			  | expression LESSEQUAL expression
-			  | expression DIFFERENT expression
-			  | expression EQUIVALENT expression
-			  | BOOLEAN
-	'''
-	if (len (p) == 4):
-		if(p[2] == '>' ):
-			p[0] = BinaryOp(">",p[1],p[3])
-		elif (p[2] == '<'):
-			p[0] = BinaryOp("<",p[1],p[3])
-		elif (p[2] == '>=' ):
-			p[0] = BinaryOp(">=",p[1],p[3])
-		elif (p[2] == '<='):
-			p[0] = BinaryOp("<=",p[1],p[3])
-		elif (p[2] == '!='):
-			p[0] = BinaryOp("!=",p[1],p[3])
-		else:
-			p[0] = BinaryOp("==",p[1],p[3])
-	else:
-		p[0] = Boolean(p[1])
+    '''
+    bool_expr : expression GREATER expression
+              | expression LESS expression
+              | expression GREATEREQUAL expression
+              | expression LESSEQUAL expression
+              | expression DIFFERENT expression
+              | expression EQUIVALENT expression
+              | BOOLEAN
+    '''
+    if (len (p) == 4):
+        if(p[2] == '>' ):
+            p[0] = BinaryOp(">",p[1],p[3])
+        elif (p[2] == '<'):
+            p[0] = BinaryOp("<",p[1],p[3])
+        elif (p[2] == '>=' ):
+            p[0] = BinaryOp(">=",p[1],p[3])
+        elif (p[2] == '<='):
+            p[0] = BinaryOp("<=",p[1],p[3])
+        elif (p[2] == '!='):
+            p[0] = BinaryOp("!=",p[1],p[3])
+        else:
+            p[0] = BinaryOp("==",p[1],p[3])
+    else:
+        p[0] = Boolean(p[1])
 
 ########
 # BinaryOp para comparaciones, retorna boolean
@@ -184,37 +184,37 @@ def bool_expr(p):
 
 
 def p_expression(p):
-	'''
-	expression : expression PLUS prod
-			   | expression MINUS prod
-			   | prod			   
-	'''
-	if(len(p) == 4):
-		if(p[2]== '+'):
-			p[0] = Operation("+",p[1],p[3]) #Esto se puede meter en BinaryOP
-		else:
-			p[0] = Operation("-",p[1],p[3])
-	else:
-		p[0] =  Prod(p[1]) #voy a hacer el nodo de esto, pero creo que se puede hacer con un nodo propio (expression)
+    '''
+    expression : expression PLUS prod
+               | expression MINUS prod
+               | prod               
+    '''
+    if(len(p) == 4):
+        if(p[2]== '+'):
+            p[0] = Operation("+",p[1],p[3]) #Esto se puede meter en BinaryOP
+        else:
+            p[0] = Operation("-",p[1],p[3])
+    else:
+        p[0] =  Prod(p[1]) #voy a hacer el nodo de esto, pero creo que se puede hacer con un nodo propio (expression)
 
 def p_prod(p):
-	'''
-	prod : prod TIMES term
-	 	 | prod DIVIDE term
-		 | term
-	'''
-	if(len(p) == 4):
-		if(p[2]== '*'):
-			p[0] = Operation("*",p[1],p[3])
-		else:
-			p[0] = Operation("/",p[1],p[3])
-	else:
-		p[0] =  Term(p[1]) #voy a hacer el nodo de esto, pero creo que se puede hacer con un nodo propio 
+    '''
+    prod : prod TIMES term
+          | prod DIVIDE term
+         | term
+    '''
+    if(len(p) == 4):
+        if(p[2]== '*'):
+            p[0] = Operation("*",p[1],p[3])
+        else:
+            p[0] = Operation("/",p[1],p[3])
+    else:
+        p[0] =  Term(p[1]) #voy a hacer el nodo de esto, pero creo que se puede hacer con un nodo propio 
 
 ############
 # la expresion dentro de parentesis debe estar en expresion no en term, es decir,
 # expression : LPAREN expression RPARENT
-#			 | las otras cosas
+#             | las otras cosas
 #
 # Que es INT_TYPE LPAREN ID RPAREN  y el otro?
 # Para que los parentesis ? para cuando hay toda una expresion encerrada entre parentesis
@@ -222,85 +222,85 @@ def p_prod(p):
 ###########
 
 def p_term(p):
-	'''
-	term : ID
-		 | FLOAT
-		 | INTEGER
-	'''
-	#toca individual para crear los nodos adecuados... No creo, simplemente un value no?, a la final esto ya e suna hojita no?
+    '''
+    term : ID
+         | FLOAT
+         | INTEGER
+    '''
+    #toca individual para crear los nodos adecuados... No creo, simplemente un value no?, a la final esto ya e suna hojita no?
 
 
 def p_return(p):
-	'''
-	return : RETURN expression
-	'''
-	p[0] = Return(p[2]) 
+    '''
+    return : RETURN expression
+    '''
+    p[0] = Return(p[2]) 
 
 def p_print_d_e(p):
-	'''
-	print_d : PRINT LPARENT expression RPARENT 
-	''' #falta imprimir con ID, y d enuevo creo que todo s epued ehacer con un mismo nodo con Value... peor voy a hacer todos
-	p[0] = PrintExpression(p[3])
+    '''
+    print_d : PRINT LPARENT expression RPARENT 
+    ''' #falta imprimir con ID, y d enuevo creo que todo s epued ehacer con un mismo nodo con Value... peor voy a hacer todos
+    p[0] = PrintExpression(p[3])
 
 def p_print_d_s(p):
-	'''
-	print_d : PRINT LPARENT STRNG RPARENT
-	'''
-	p[0] = PrintString(p[3])
+    '''
+    print_d : PRINT LPARENT STRNG RPARENT
+    '''
+    p[0] = PrintString(p[3])
 
 def p_call_d(p):
-	'''
-	call_d : ID LPAREN list_var RPARENT
-	'''
-	p[0] = Call_d(p[1],p[3])
-	#Nombre y argumentos funcion
+    '''
+    call_d : ID LPAREN list_var RPARENT
+    '''
+    p[0] = Call_d(p[1],p[3])
+    #Nombre y argumentos funcion
 
 def list_var(p):
-	'''
-	list_var : ID list_var
-			 | expression list_var
-			 | empty
-	'''
-	#terminar, separar
+    '''
+    list_var : ID list_var
+             | expression list_var
+             | empty
+    '''
+    #terminar, separar
 
 def type(p):
-	'''
-	type : INT_TYPE
-		 | FLOAT_TYPE
-		 | STRING_TYPE
-		 | BOOLEAN_TYPE
-	'''
-	#terminar, separar para crear nodos del tipo correspondiente
+    '''
+    type : INT_TYPE
+         | FLOAT_TYPE
+         | STRING_TYPE
+         | BOOLEAN_TYPE
+    '''
+    #terminar, separar para crear nodos del tipo correspondiente
 
 def line_if(p):
-	'''
-	line_if : IF conditional THEN instructrion
-	'''
-	p[0] =  Line_if(p[2],p[4])
+    '''
+    line_if : IF conditional THEN instructrion
+    '''
+    p[0] =  Line_if(p[2],p[4])
 
 def line_while(p):
-	'''
-	line_while : WHILE conditional DO instruction
-	'''
-	p[0] =  Line_while(p[2],p[4])
+    '''
+    line_while : WHILE conditional DO instruction
+    '''
+    p[0] =  Line_while(p[2],p[4])
 
 def assignation(p):
-	'''
-	assignation : ID EQUAL expression
-	'''
-	p[0] = Assignation(p[1],p[3])
-	#se puede convertir en nodo var, a esto le faltan cosas: ID=ID, faltan var_dec_as vendrian siendo ctes
+    '''
+    assignation : ID EQUAL expression
+    '''
+    p[0] = Assignation(p[1],p[3])
+    #se puede convertir en nodo var, a esto le faltan cosas: ID=ID, faltan var_dec_as vendrian siendo ctes
 
 
 def p_error(p):
-	if p:
-		error(p.lineno, "Error de sintaxis en la linea")
+    if p:
+        error(p.lineno, "Error de sintaxis en la linea")
 
 if __name__=='__main__':
-	import mpaslex
-	import sys
-	from errors import suscribe_errors
-	lexer = mpaslex.make_lexer()
-	parser = make_parser()
+    import mpaslex
+    import sys
+    from errors import suscribe_errors
+    lexer = mpaslex.make_lexer()
+    parser = make_parser()
 
-		
+        
