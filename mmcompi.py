6b0b9a2b968c0846066f5e96a7a2663f7a6a7632
pin3da <manuel.felipe.pineda@gmail.com>
2013-06-02 18:35:06 -0500

    rm unnecessary files

--- mmcompi.py
+++ mmcompi.py
@@ -1,286 +1,177 @@
-# mpascode.py
-# -*- coding: utf-8 -*-
-
 import mpasast
 import mpasblock
+from mpasast import *
 from mpasblock import BasicBlock, IfBlock, WhileBlock
 from collections import defaultdict
 
 binary_ops = {
-	'+' : 'add',
-	'-' : 'sub',
-	'*' : 'mul',
-	'/' : 'div',
-	'<' : 'lt',
-	'>' : 'gt',
-	'==': 'eq',
-	'!=': 'ne',
-	'<=': 'le',
-	'>=': 'ge',
-	'&&': 'land',
-	'||': 'lor',
+    '+' : 'add',
+    '-' : 'sub',
+    '*' : 'mul',
+    '/' : 'div',
+    '<' : 'lt',
+    '>' : 'gt',
+    '==': 'eq',
+    '!=': 'ne',
+    '<=': 'le',
+    '>=': 'ge',
+    'and': 'land',
+    'or': 'lor',
 }
 
 unary_ops = {
-	'+' : 'uadd',
-	'-' : 'usub',
-	'!' : 'lnot',
+    '+' : 'uadd',
+    '-' : 'usub',
+    '!' : 'lnot',
 }
 
-class GenerateCode(mpasast.NodeVisitor):
-	'''
-	Clase Visitor Node que crea secuencias de instrucciones de 3-direcciones.
-	'''
-	def __init__(self):
-		super(GenerateCode, self).__init__()
+class SSAVisitor(mpasast.NodeVisitor):
+    '''
+    Clase Visitor Node que crea secuencias de instrucciones de 3-direcciones.
+    '''
+    def __init__(self):
+        super(SSAVisitor, self).__init__()
 
-		# version diccionario para temporales
-		self.versions = defaultdict(int)
+        # version diccionario para temporales
+        self.versions = defaultdict(int)
 
-		# El código generado (lista de tuplas)
-		self.code = BasicBlock()
-		self.start_block = self.code
+        # El codigo generado (lista de tuplas)
+        self.code = BasicBlock()
+        self.start_block = self.code
 
-		# Una lista de declaraciones externas (y tipos)
-		self.externs = []
+        # Una lista de declaraciones externas (y tipos)
+        self.externs = []
 
-	def new_temp(self,typeobj):
-		'''
-		Crea una nueva variable temporal de un tipo dado.
-		'''
-		name = "__%s_%d" % (typeobj.name, self.versions[typeobj.name])
-		self.versions[typeobj.name] += 1
-		return name
+    def new_temp(self,return_type):
+        '''
+        Crea una nueva variable temporal de un tipo dado.
+        '''
+        name = "__%s_%d" % (return_type, self.versions[return_type])
+        self.versions[return_type] += 1
+        return name
+    #visit definitions start here
+    
+    def visit_Var_dec(self, node):
+        # localice en memoria
+        inst = ('alloc_'+node.return_type, node.id)
+        self.code.append(inst)
+        #se borro el resto pq nosotros en var_dec solo alocamos memoria no mas
+        
+        
+    def visit_Type(self,_type):
+        pass
+        
+    def visit_Assignation(self, node):
+        self.visit(node.value)
+        if not (isinstance(node.ubication, Ubication_vector)):
+            inst = ('store_'+node.value.return_type, node.value.gen_location, node.ubication.value)
+            self.code.append(inst)
+        else:
+            inst = ('store_'+node.value.return_type, node.value.gen_location, node.ubication.id)
+            self.code.append(inst) 
+        
+    def visit_Print(self, node):
+        if(isinstance(node.value, AST)):
+            self.visit(node.value)
+            inst = ('print_string','expression')
+        else:
+            inst = ('print_string',node.value)
 
-	# Debe implementar métodos visit_Nodename para todos los demas nodos
-	# del AST.  En su código, se tendrá que hacer las instrucciones y
-	# agregarlas a la lista self.code.
-	#
-	# Siguen algunos métodos de ejemplo.  Deberá ajustarlos dependiendo
-	# de los nombres de los nodos AST que se haya definido.
+        # Cree el opcode y agregarlo a la lista
+        self.code.append(inst)
+        
+        
+    def visit_Write(self,node):
+        pass
+        
+    def visit_Binary_op(self,node):
+        # Visita las expresiones izquierda y derecha
+        self.visit(node.left)
+        self.visit(node.right)
+        # Crea un nuevo temporal para almacenar el resultado
+        target = self.new_temp(node.return_type)
 
-	def visit_Literal(self,node):
-		# Crea un nuevo nombre de variable temporal
-		target = self.new_temp(node.type)
+        # Crea opcode y agrega a la lista
+        opcode = binary_ops[node.op] + "_" + node.return_type
+        inst = (opcode, node.left.gen_location, node.right.gen_location, target)
+        self.code.append(inst)
 
-		# Crea el opcode SSA y lo agrega a la lista de instrucciones generadas
-		inst = ('literal_'+node.type.name, node.value, target)
-		self.code.append(inst)
+        # Almacena localizacion del resultado en el nodo
+        node.gen_location = target
+        
+        
+    def visit_Unary_op(self, node):
+        self.visit(node.value)
+        target = self.new_temp(node.return_type)
+        opcode = unary_ops[node.op] + "_" + node.value.return_type
+        inst = (opcode, node.value.gen_location)
+        self.code.append(inst)
+        node.gen_location = target
+     
+    def visit_Integer(self, node):
+        target =  self.new_temp('integer')
+        node.gen_location = target
+        inst = ('literal_integer', node.value, target)
+        self.code.append(inst)
+                
+    def visit_Float(self,node):
+        target = self.new_temp('float')
+        node.gen_location =  target
+        inst = ('literal_float', node.value, target)
+        self.code.append(inst)
 
-		# Graba el nombre de la variable temporal donde el valor fue colocado
-		node.gen_location = target
-
-	def visit_BinaryOp(self,node):
-		# Visita las expresiones izquierda y derecha
-		self.visit(node.left)
-		self.visit(node.right)
-
-		# Crea un nuevo temporal para almacenar el resultado
-		target = self.new_temp(node.type)
-
-		# Crea opcode y agrega a la lista
-		opcode = binary_ops[node.op] + "_"+node.left.type.name
-		inst = (opcode, node.left.gen_location, node.right.gen_location, target)
-		self.code.append(inst)
-
-		# Almacena localizacion del resultado en el nodo
-		node.gen_location = target
-
-	def visit_RelationalOp(self,node):
-		# Visit las expresiones izquierda y derecha
-		self.visit(node.left)
-		self.visit(node.right)
-
-		# Cree un temporal nuevo para almacer el resultado
-		target = self.new_temp(node.type)
-
-		# Cree el opcode y agregarlo a la lista
-		#opcode = binary_ops[node.op] + "_"+node.left.type.name
-		opcode = "cmp" + "_"+node.left.type.name
-		inst = (opcode, binary_ops[node.op], node.left.gen_location, node.right.gen_location, target)
-		self.code.append(inst)
-
-		# Almacene localizacion del resultado al nodo
-		node.gen_location = target
-
-	def visit_PrintStatement(self,node):
-		# Visit la expresion print
-		self.visit(node.expr)
-
-		# Cree el opcode y agregarlo a la lista
-		inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-		self.code.append(inst)
-
-	def visit_Program(self,node):
-		self.visit(node.program)
-
-	#def visit_Statements(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	#def visit_Statement(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	def visit_ConstDeclaration(self,node):
-		# localice en memoria
-		inst = ('alloc_'+node.type.name, 
-		        node.id)
-		self.code.append(inst)
-		# almacene valor inicial
-		self.visit(node.value)
-		inst = ('store_'+node.type.name,
-		        node.value.gen_location,
-		        node.id)
-		self.code.append(inst)
-
-	def visit_VarDeclaration(self,node):
-		# localice en memoria
-		inst = ('alloc_'+node.type.name, 
-		        node.id)
-		self.code.append(inst)
-		# almacene pot. val inicial
-		if node.value:
-		    self.visit(node.value)
-		    inst = ('store_'+node.type.name,
-		            node.value.gen_location,
-		            node.id)
-		    self.code.append(inst)
-
-	def visit_LoadLocation(self,node):
-		target = self.new_temp(node.type)
-		inst = ('load_'+node.type.name,
-		        node.name,
-		        target)
-		self.code.append(inst)
-		node.gen_location = target
-
-	#def visit_Extern(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	#def visit_FuncPrototype(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	#def visit_Parameters(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-	#    node.gen_location = target
-
-	#def visit_ParamDecl(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	def visit_AssignmentStatement(self,node):
-		self.visit(node.value)
-		
-		inst = ('store_'+node.value.type.name, 
-		        node.value.gen_location, 
-		        node.location)
-		self.code.append(inst)
-
-	def visit_UnaryOp(self,node):
-		self.visit(node.left)
-		target = self.new_temp(node.type)
-		opcode = unary_ops[node.op] + "_" + node.left.type.name
-		inst = (opcode, node.left.gen_location)
-		self.code.append(inst)
-		node.gen_location = target
-
-	def visit_IfStatement(self,node):
-		if_block = IfBlock()
-		self.code.next_block = if_block
-		# condition
-		self.switch_block(if_block)
-		self.visit(node.condition)
-		if_block.test = node.condition.gen_location
-		# then branch
-		if_block.if_branch = BasicBlock()
-		self.switch_block(if_block.if_branch)
-		self.visit(node.then_b)
-		# else branch
-		if node.else_b:
-		    if_block.else_branch = BasicBlock()
-		    self.switch_block(if_block.else_branch)
-		    self.visit(node.else_b)
-		# fija el siguiente bloque
-		if_block.next_block = BasicBlock()
-		self.switch_block(if_block.next_block)
-
-	def visit_WhileStatement(self, node):
-		while_block = WhileBlock()
-		self.code.next_block = while_block
-		# condition
-		self.switch_block(while_block)
-		self.visit(node.condition)
-		while_block.test = node.condition.gen_location
-		# body
-		while_block.body = BasicBlock()
-		self.switch_block(while_block.body)
-		self.visit(node.body)
-		while_block.next_block = BasicBlock()
-		self.switch_block(while_block.next_block)
-
-	def switch_block(self, next_block):
-		self.code = next_block
-
-	def visit_Group(self,node):
-		self.visit(node.expression)
-		node.gen_location = node.expression.gen_location
-
-	#def visit_FunCall(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
-
-	#def visit_ExprList(self,node):
-	#    self.visit(node.expr)
-	#    inst = ('print_'+node.expr.type.name, node.expr.gen_location)
-	#    self.code.append(inst)
+    def visit_Call_func(self, node):
+        target = self.new_temp(node.return_type)
+        inst = ('call_func',node.func_id,target)
+        self.code.append(inst)
+        node.gen_location = target
 
 
-# STEP 3: Probar
-# 
-# Trate de correr este programa con un archivo adecuado para tal efecto y vea
-# la secuencia del codigo SSA resultante.
-#
-#     bash % python mpascode.py good.pas
-#     ... vea la salida ...
-#
-# ----------------------------------------------------------------------
-#            NO MODIFIQUE NADA DE AQUI EN ADELANTE
-# ----------------------------------------------------------------------
+
+    ####
+    # Terminar allocator vector
+    ###
+    
+    def visit_Vector(self,node):
+        pass
+    
+    def visit_Ubication_vector(self,node):
+        pass
+
+    def visit_Id(self,node):
+        target =  self.new_temp(node.return_type)
+        inst=('load_'+node.return_type, node.value, target)
+        node.gen_location=target
+        self.code.append(inst)
+
+
 def generate_code(node):
-	'''
-	Genera código SSA desde el nodo AST entregado.
-	'''
-	gen = GenerateCode()
-	gen.visit(node)
-	return gen
+    '''
+    Genera codigo SSA desde el nodo AST entregado.
+    '''
+    gen = SSAVisitor()
+    gen.visit(node)
+    return gen
 
 if __name__ == '__main__':
-	import mpaslex
-	import mpasparse
-	import mpascheck
-	import sys
-	from errors import subscribe_errors, errors_reported
-	lexer = mpaslex.make_lexer()
-	parser = mpasparse.make_parser()
-	with subscribe_errors(lambda msg: sys.stdout.write(msg+"\n")):
-		program = parser.parse(open(sys.argv[1]).read())
-		# Revise el programa
-		exprcheck.check_program(program)
-		# Si no ocurre errore, genere código
-		if not errors_reported():
-			code = generate_code(program)
-			# Emite la secuencia de código
-			exprblock.PrintBlocks().visit(code.start_block)
-			#for inst in code.code:
-			#    print(inst)
+    import mpaslex
+    import mpasparse
+    import semantic
+    import sys
+    from errors import subscribe_errors, errors_reported
+    lexer = mpaslex.make_lexer()
+    parser = mpasparse.make_parser()
+    with subscribe_errors(lambda msg: sys.stdout.write(msg+"\n")):
+        program = parser.parse(open(sys.argv[1]).read())
+        # Revise el programa
+        num_errors = semantic.check_program(program)
+        # Si no ocurre errore, genere codigo
+        if num_errors == 0:
+            code = generate_code(program)
+            # Emite la secuencia de codigo
+            #exprblock.PrintBlocks().visit(code.start_block)
+            
+            for inst in code.code:
+                print(inst)
+        else:
+            sys.stderr.write("Program couldn't be compiled\n")
